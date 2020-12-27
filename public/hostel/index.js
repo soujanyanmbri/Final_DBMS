d16e9b4c48b278ca9208837d42ae705355b4353f
HotMonkeyWings <devsony52@gmail.com>
2020-12-27 22:27:30 +0530

    Updated hostels to our DB, Got rid of Edit for now

--- public/hostel/index.js
+++ public/hostel/index.js
@@ -2,63 +2,55 @@
  * Created by bhavyaagg on 01/04/18.
  */
 
-$(function () {
+$(function() {
 
-  $.get('/api/hostel/viewAll', function (response) {
-    if (response.success) {
-      let hostels = response.data;
+    $.get('/api/hostel/viewAll', function(response) {
+        if (response.success) {
+            let hostels = response.data;
 
-      let hostelsList = $('#hostelsList');
+            let hostelsList = $('#hostelsList');
 
-      for (let i = 0; i < hostels.length; i++) {
+            for (let i = 0; i < hostels.length; i++) {
 
-        hostelsList.append(`<li class="list-group-item">
+                hostelsList.append(`<li class="list-group-item">
         <div class="row text-center">
-        <div class="col-3">${hostels[i].name}</div>
-        <div class="col-6">${hostels[i].capacity}</div>
-      <div class="col-3">
-        E/X
-        </div>
+        <div class="col-2">${hostels[i].Hostel_Name}</div>
+        <div class="col-2">${hostels[i].No_of_Rooms}</div>
+        <div class="col-2">${hostels[i].No_of_floors}</div>
+        <div class="col-2">${hostels[i].Location}</div>
+        <div class="col-2">${hostels[i].Elevator}</div>
+
         </div>
         </li>`)
-      }
-    }
-  })
-
-  // $('#addHostelBtn').click(() => {
-  //   $('#listHostels').empty()
-  //   $('#addHostelDiv').css("display", "block");
-  // })
-
-  $('#submitHostelBtn').click(function () {
-
-    $.post('/api/hostel/add',
-      {
-        name: $('#name').val(),
-        capacity: $('#capacity').val()
-      },
-      function (response) {
-        if (response.success) {
-          $('#addHostelModal').modal('hide');
-          window.location.reload();
+            }
         }
-        else {
-          console.log("could not add the hostel right now")
-        }
-      })
+    })
 
-  })
+    // $('#addHostelBtn').click(() => {
+    //   $('#listHostels').empty()
+    //   $('#addHostelDiv').css("display", "block");
+    // })
 
-  // $('#viewAllHostelsBtn').click(function () {
-  //   $('#addHostelDiv').css("display", "none");
-  //   $.get('/api/hostel/viewAll', function (data) {
-  //     $('#listHostels').empty()
-  //
-  //     data.forEach((hostel) => {
-  //       console.log(hostel);
-  //       $('#listHostels').append(`<li>hid: ${hostel.hid} | name: ${hostel.name} | capacity: ${hostel.capacity}</li>`)
-  //     })
-  //   })
-  // })
+    $('#submitHostelBtn').click(function() {
+
+        $.post('/api/hostel/add', {
+                Hostel_Name: $('#Hostel_Name').val(),
+                No_of_Rooms: $('#No_of_Rooms').val(),
+                No_of_floors: $('#No_of_floors').val(),
+                Location: $('#Location').val(),
+                Elevator: $('#Elevator').val()
+            },
+            function(response) {
+                if (response.success) {
+                    $('#addHostelModal').modal('hide');
+                    window.location.reload();
+                } else {
+                    console.log("Could not add the hostel right now")
+                }
+            })
+
+    })
+
+
 
 });
